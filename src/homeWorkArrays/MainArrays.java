c0349a8c4c261a1739032a90e9212f39a73483ea
khaganii <Cavidan1Tunar2>
2022-08-01 15:22:07 +0400

    homework arrays added

---
+++ src/homeWorkArrays/MainArrays.java
@@ -0,0 +1,59 @@
+package homeWorkArrays;
+
+public class MainArrays {
+    public static void main(String[] args) {
+        int[] numbers = {23, 44, 21, 31, 42};
+        //System.out.println(calculateSumOfElementsOfArray(numbers));
+        //System.out.println(calculateNumericalAverageOfElementsOfArray(numbers));
+        //System.out.println(findMinimumElementOfArray(numbers));
+        //printOddElementsOfArray(numbers);
+        //printEvenElementsOfArray(numbers);
+        printPrimeElementsOfArray(numbers);
+    }
+
+    private static void printPrimeElementsOfArray(int[] arr) {
+        for (int num : arr)
+            if (isPrime(num))
+                System.out.println(num);
+    }
+
+    private static boolean isPrime(int num){
+        int dividerCount = 0;
+        for (int i = 1; i <= num/2; i++)
+            if (num % i == 0)
+                dividerCount++;
+        return (dividerCount==1);
+    }
+
+    private static void printEvenElementsOfArray(int[] arr) {
+        for (int num : arr)
+            if (num%2 == 1)
+                System.out.println(num);
+    }
+
+    private static void printOddElementsOfArray(int[] arr) {
+        for (int num : arr)
+            if (num%2 == 0)
+                System.out.println(num);
+    }
+
+    public static int calculateSumOfElementsOfArray(int[] arr){
+        int sum = 0;
+        for (int num:arr) sum += num;
+        return sum;
+    }
+
+    public static int calculateNumericalAverageOfElementsOfArray(int[] arr){
+        int sum = 0;
+        for (int num:arr) sum += num;
+        return sum/arr.length;
+    }
+
+    public static int findMinimumElementOfArray(int[] arr){
+        int min = Integer.MAX_VALUE;
+        for (int num : arr)
+            min = Math.min(num, min);
+        return min;
+    }
+
+}
