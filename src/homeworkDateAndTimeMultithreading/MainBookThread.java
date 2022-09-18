1015125351f34767933d626fc474bdbc29cc0158
khaganii <Cavidan1Tunar2>
2022-09-18 13:07:13 +0400

    sql added

--- src/homeworkDateAndTimeMultithreading/MainBookThread.java
+++ src/homeworkDateAndTimeMultithreading/MainBookThread.java
@@ -19,5 +19,8 @@ public class MainBookThread {
 
     private static void showBookPrice(Book b) {
         System.out.println("Price = " + b.getPrice());
+        synchronized ("dc"){
+
+        }
     }
 }
